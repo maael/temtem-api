e3eecc8b5f2fd5512d279a17313e283fe367941a
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-01-27 15:50:53 +0000

    feat: 🎸 Add more info to doc page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Jumbrotron from '@maael/jumbotron-component';
 import ApiBlock from '@maael/api-block-component';
 import ApiHeader from '@maael/api-header-component';
@@ -13,59 +14,150 @@ export default () => (
       }
     `}</style>
     <Jumbrotron>
-      <div>Temtem API</div>
+      <div style={{textAlign: 'center'}}>
+        <div style={{fontWeight: 'bold', marginBottom: 5}}>Temtem API</div>
+        <div>JSON data from the official wiki, updated every hour</div>
+      </div>
     </Jumbrotron>
-    <ApiBlock example={examples.knownTemtemExample}>
-      <>
-        <ApiHeader path='/api/known-temtems' />
-        <ApiParamBlock params={[
-          {name: 'names', required: false, description: ''},
-          {name: 'fields', required: false, description: ''},
-          {name: 'expand', required: false, description: ''}
-        ]} />
-      </>
-    </ApiBlock>
-    <ApiBlock example={examples.typeExample}>
-      <ApiHeader path='/api/types' />
-    </ApiBlock>
-    <ApiBlock example={examples.conditionExample}>
-      <ApiHeader path='/api/conditions' />
-    </ApiBlock>
-    <ApiBlock example={examples.techniqueExample}>
-      <>
-        <ApiHeader path='/api/techniques' />
-        <ApiParamBlock params={[
-          {name: 'names', required: false, description: ''},
-          {name: 'fields', required: false, description: ''},
-        ]} />
-      </>
-    </ApiBlock>
-    <ApiBlock example={examples.traitExample}>
-      <>
-        <ApiHeader path='/api/traits' />
-        <ApiParamBlock params={[
-          {name: 'names', required: false, description: ''},
-          {name: 'fields', required: false, description: ''},
-        ]} />
-      </>
-    </ApiBlock>
-    <ApiBlock example={examples.gearExample}>
-      <ApiHeader path='/api/gear' />
-    </ApiBlock>
-    <ApiBlock example={examples.weaknessesExample}>
-      <ApiHeader path='/api/weaknesses' />
-    </ApiBlock>
-    <ApiBlock example={examples.weaknessCalculateExample}>
-      <>
-        <ApiHeader path='/api/weaknesses/calculate' />
-        <ApiParamBlock params={[
-          {name: 'attacking', required: false, description: ''},
-          {name: 'defending', required: false, description: ''},
-        ]} />
-      </>
-    </ApiBlock>
-    <ApiBlock example={examples.breedingExample}>
-      <ApiHeader path='/api/breeding' />
-    </ApiBlock>
+    <KnownTemtemsBlock />
+    <TypesBlock />
+    <ConditionsBlock />
+    <TechniquesBlock />
+    <TraitsBlock />
+    <GearBlock />
+    <WeaknessesBlock />
+    <CalculateBlock />
+    <BreedingBlock />
   </>
 );
+
+function useNum (url: string) {
+  const [num, setNum] = useState(0);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(url);
+        setNum((await res.json()).length);
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+  return num;
+}
+
+function KnownTemtemsBlock () {
+  const num = useNum('/api/known-temtems');
+  return (
+    <ApiBlock example={examples.knownTemtemExample}>
+      <>
+        <ApiHeader path='/api/known-temtems' style={{marginBottom: 10}} />
+        <ApiParamBlock style={{margin: 10}} params={[
+          {name: 'names', required: false, description: 'A comma separated list of the Temtem names that you want information about.'},
+          {name: 'fields', required: false, description: 'A comma separated list of fields you want returned'},
+          {name: 'expand', required: false, description: 'A comma separated list of fields you want extended.'}
+        ]} />
+        <p>Currently has information for {num} Temtems.</p>
+        <p>You can extend the <b>trait</b>, <b>technique</b>, and <b>type</b> fields, which then means they will return an array of data in the shape returned by the endpoints below instead of the shape shown here.</p>
+      </>
+    </ApiBlock>
+  )
+}
+
+function TypesBlock () {
+  const num = useNum('/api/types');
+  return (
+    <ApiBlock example={examples.typeExample}>
+      <>
+        <ApiHeader path='/api/types' style={{marginBottom: 10}} />
+        <p>Currently has information for {num} types.</p>
+      </>
+    </ApiBlock>
+  );
+}
+
+function ConditionsBlock () {
+  const num = useNum('/api/conditions');
+  return (
+    <ApiBlock example={examples.conditionExample}>
+     <>
+        <ApiHeader path='/api/conditions' style={{marginBottom: 10}} />
+        <p>Currently has information for {num} conditions.</p>
+      </>
+    </ApiBlock>
+  );
+}
+
+function TechniquesBlock () {
+  const num = useNum('/api/techniques');
+  return (
+    <ApiBlock example={examples.techniqueExample}>
+      <>
+        <ApiHeader path='/api/techniques' style={{marginBottom: 10}} />
+        <ApiParamBlock style={{margin: 10}} params={[
+          {name: 'names', required: false, description: 'A comma separated list of technique names that you want information about.'},
+          {name: 'fields', required: false, description: 'A comma separated list of fields you want returned.'},
+        ]} />
+        <p>Currently has information for {num} techniques.</p>
+      </>
+    </ApiBlock>
+  );
+}
+
+function TraitsBlock () {
+  const num = useNum('/api/traits');
+  return (
+    <ApiBlock example={examples.traitExample}>
+      <>
+        <ApiHeader path='/api/traits' style={{marginBottom: 10}} />
+        <ApiParamBlock style={{margin: 10}} params={[
+          {name: 'names', required: false, description: 'A comma separated list of trait names that you want information about.'},
+          {name: 'fields', required: false, description: 'A comma separated list of fields you want returned.'},
+        ]} />
+        <p>Currently has information for {num} traits.</p>
+      </>
+    </ApiBlock>
+  );
+}
+
+function GearBlock () {
+  const num = useNum('/api/gear');
+  return (
+    <ApiBlock example={examples.gearExample}>
+      <>
+        <ApiHeader path='/api/gear' style={{marginBottom: 10}} />
+        <p>Currently has information for {num} pieces of gear.</p>
+      </>
+    </ApiBlock>
+  );
+}
+
+function WeaknessesBlock () {
+  return (
+    <ApiBlock example={examples.weaknessesExample}>
+      <ApiHeader path='/api/weaknesses' style={{marginBottom: 10}} />
+    </ApiBlock>
+  );
+}
+
+function CalculateBlock () {
+  return (
+    <ApiBlock example={examples.weaknessCalculateExample}>
+      <>
+        <ApiHeader path='/api/weaknesses/calculate' style={{marginBottom: 10}} />
+        <ApiParamBlock style={{margin: 10}} params={[
+          {name: 'attacking', required: false, description: 'A valid Temtem type to use as the attack value.'},
+          {name: 'defending', required: false, description: 'A comma separated list of valid Temtem types to use as the defending values'},
+        ]} />
+      </>
+    </ApiBlock>
+  );
+}
+
+function BreedingBlock () {
+  return (
+    <ApiBlock example={examples.breedingExample}>
+      <ApiHeader path='/api/breeding' style={{marginBottom: 10}} />
+    </ApiBlock>
+  );
+}
