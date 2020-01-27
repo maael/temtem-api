f687fc5f1dc5b4931be5127d3c44ea29d0cc4cf6
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-01-27 15:51:45 +0000

    style: 💄 Prettier
    I need to do this as a hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from 'react';
-import Jumbrotron from '@maael/jumbotron-component';
-import ApiBlock from '@maael/api-block-component';
-import ApiHeader from '@maael/api-header-component';
-import ApiParamBlock from '@maael/api-param-block-component';
-import * as examples from '../util/examples';
+import { useEffect, useState } from "react";
+import Jumbrotron from "@maael/jumbotron-component";
+import ApiBlock from "@maael/api-block-component";
+import ApiHeader from "@maael/api-header-component";
+import ApiParamBlock from "@maael/api-param-block-component";
+import * as examples from "../util/examples";
 
 export default () => (
   <>
     <style jsx global>{`
-      html, body {
+      html,
+      body {
         padding: 0;
         margin: 0;
       }
     `}</style>
     <Jumbrotron>
-      <div style={{textAlign: 'center'}}>
-        <div style={{fontWeight: 'bold', marginBottom: 5}}>Temtem API</div>
+      <div style={{ textAlign: "center" }}>
+        <div style={{ fontWeight: "bold", marginBottom: 5 }}>Temtem API</div>
         <div>JSON data from the official wiki, updated every hour</div>
       </div>
     </Jumbrotron>
@@ -31,7 +32,7 @@ export default () => (
   </>
 );
 
-function useNum (url: string) {
+function useNum(url: string) {
   const [num, setNum] = useState(0);
   useEffect(() => {
     (async () => {
@@ -46,118 +47,177 @@ function useNum (url: string) {
   return num;
 }
 
-function KnownTemtemsBlock () {
-  const num = useNum('/api/known-temtems');
+function KnownTemtemsBlock() {
+  const num = useNum("/api/known-temtems");
   return (
     <ApiBlock example={examples.knownTemtemExample}>
       <>
-        <ApiHeader path='/api/known-temtems' style={{marginBottom: 10}} />
-        <ApiParamBlock style={{margin: 10}} params={[
-          {name: 'names', required: false, description: 'A comma separated list of the Temtem names that you want information about.'},
-          {name: 'fields', required: false, description: 'A comma separated list of fields you want returned.'},
-          {name: 'expand', required: false, description: 'A comma separated list of fields you want extended.'}
-        ]} />
+        <ApiHeader path="/api/known-temtems" style={{ marginBottom: 10 }} />
+        <ApiParamBlock
+          style={{ margin: 10 }}
+          params={[
+            {
+              name: "names",
+              required: false,
+              description:
+                "A comma separated list of the Temtem names that you want information about."
+            },
+            {
+              name: "fields",
+              required: false,
+              description: "A comma separated list of fields you want returned."
+            },
+            {
+              name: "expand",
+              required: false,
+              description: "A comma separated list of fields you want extended."
+            }
+          ]}
+        />
         <p>Currently has information for {num} Temtems.</p>
-        <p>You can extend the <b>trait</b>, <b>technique</b>, and <b>type</b> fields, which then means they will return an array of data in the shape returned by the endpoints below instead of the shape shown here.</p>
+        <p>
+          You can extend the <b>trait</b>, <b>technique</b>, and <b>type</b>{" "}
+          fields, which then means they will return an array of data in the
+          shape returned by the endpoints below instead of the shape shown here.
+        </p>
       </>
     </ApiBlock>
-  )
+  );
 }
 
-function TypesBlock () {
-  const num = useNum('/api/types');
+function TypesBlock() {
+  const num = useNum("/api/types");
   return (
     <ApiBlock example={examples.typeExample}>
       <>
-        <ApiHeader path='/api/types' style={{marginBottom: 10}} />
+        <ApiHeader path="/api/types" style={{ marginBottom: 10 }} />
         <p>Currently has information for {num} types.</p>
       </>
     </ApiBlock>
   );
 }
 
-function ConditionsBlock () {
-  const num = useNum('/api/conditions');
+function ConditionsBlock() {
+  const num = useNum("/api/conditions");
   return (
     <ApiBlock example={examples.conditionExample}>
-     <>
-        <ApiHeader path='/api/conditions' style={{marginBottom: 10}} />
+      <>
+        <ApiHeader path="/api/conditions" style={{ marginBottom: 10 }} />
         <p>Currently has information for {num} conditions.</p>
       </>
     </ApiBlock>
   );
 }
 
-function TechniquesBlock () {
-  const num = useNum('/api/techniques');
+function TechniquesBlock() {
+  const num = useNum("/api/techniques");
   return (
     <ApiBlock example={examples.techniqueExample}>
       <>
-        <ApiHeader path='/api/techniques' style={{marginBottom: 10}} />
-        <ApiParamBlock style={{margin: 10}} params={[
-          {name: 'names', required: false, description: 'A comma separated list of technique names that you want information about.'},
-          {name: 'fields', required: false, description: 'A comma separated list of fields you want returned.'},
-        ]} />
+        <ApiHeader path="/api/techniques" style={{ marginBottom: 10 }} />
+        <ApiParamBlock
+          style={{ margin: 10 }}
+          params={[
+            {
+              name: "names",
+              required: false,
+              description:
+                "A comma separated list of technique names that you want information about."
+            },
+            {
+              name: "fields",
+              required: false,
+              description: "A comma separated list of fields you want returned."
+            }
+          ]}
+        />
         <p>Currently has information for {num} techniques.</p>
       </>
     </ApiBlock>
   );
 }
 
-function TraitsBlock () {
-  const num = useNum('/api/traits');
+function TraitsBlock() {
+  const num = useNum("/api/traits");
   return (
     <ApiBlock example={examples.traitExample}>
       <>
-        <ApiHeader path='/api/traits' style={{marginBottom: 10}} />
-        <ApiParamBlock style={{margin: 10}} params={[
-          {name: 'names', required: false, description: 'A comma separated list of trait names that you want information about.'},
-          {name: 'fields', required: false, description: 'A comma separated list of fields you want returned.'},
-        ]} />
+        <ApiHeader path="/api/traits" style={{ marginBottom: 10 }} />
+        <ApiParamBlock
+          style={{ margin: 10 }}
+          params={[
+            {
+              name: "names",
+              required: false,
+              description:
+                "A comma separated list of trait names that you want information about."
+            },
+            {
+              name: "fields",
+              required: false,
+              description: "A comma separated list of fields you want returned."
+            }
+          ]}
+        />
         <p>Currently has information for {num} traits.</p>
       </>
     </ApiBlock>
   );
 }
 
-function GearBlock () {
-  const num = useNum('/api/gear');
+function GearBlock() {
+  const num = useNum("/api/gear");
   return (
     <ApiBlock example={examples.gearExample}>
       <>
-        <ApiHeader path='/api/gear' style={{marginBottom: 10}} />
+        <ApiHeader path="/api/gear" style={{ marginBottom: 10 }} />
         <p>Currently has information for {num} pieces of gear.</p>
       </>
     </ApiBlock>
   );
 }
 
-function WeaknessesBlock () {
+function WeaknessesBlock() {
   return (
     <ApiBlock example={examples.weaknessesExample}>
-      <ApiHeader path='/api/weaknesses' style={{marginBottom: 10}} />
+      <ApiHeader path="/api/weaknesses" style={{ marginBottom: 10 }} />
     </ApiBlock>
   );
 }
 
-function CalculateBlock () {
+function CalculateBlock() {
   return (
     <ApiBlock example={examples.weaknessCalculateExample}>
       <>
-        <ApiHeader path='/api/weaknesses/calculate' style={{marginBottom: 10}} />
-        <ApiParamBlock style={{margin: 10}} params={[
-          {name: 'attacking', required: false, description: 'A valid Temtem type to use as the attack value.'},
-          {name: 'defending', required: false, description: 'A comma separated list of valid Temtem types to use as the defending values'},
-        ]} />
+        <ApiHeader
+          path="/api/weaknesses/calculate"
+          style={{ marginBottom: 10 }}
+        />
+        <ApiParamBlock
+          style={{ margin: 10 }}
+          params={[
+            {
+              name: "attacking",
+              required: false,
+              description: "A valid Temtem type to use as the attack value."
+            },
+            {
+              name: "defending",
+              required: false,
+              description:
+                "A comma separated list of valid Temtem types to use as the defending values"
+            }
+          ]}
+        />
       </>
     </ApiBlock>
   );
 }
 
-function BreedingBlock () {
+function BreedingBlock() {
   return (
     <ApiBlock example={examples.breedingExample}>
-      <ApiHeader path='/api/breeding' style={{marginBottom: 10}} />
+      <ApiHeader path="/api/breeding" style={{ marginBottom: 10 }} />
     </ApiBlock>
   );
 }
--- util/examples.ts
+++ util/examples.ts
@@ -1,332 +1,344 @@
-export const typeExample = [{
-  "name": "Neutral",
-  "icon": "/images/icon/types/Neutral.png"
-}];
+export const typeExample = [
+  {
+    name: "Neutral",
+    icon: "/images/icon/types/Neutral.png"
+  }
+];
 
-export const conditionExample = [{
-  "name": "Cold",
-  "description": "If Cold is afflicted twice, the Temtem becomes Frozen. Frozen Temtem cannot attack, but they can rest normally. Cold and Frozen also serve to erase the Burnt Condition from a Temtem, while Burnt erases both Cold and Frozen",
-  "icon": "/images/icons/conditions/Cold.png"
-}]
+export const conditionExample = [
+  {
+    name: "Cold",
+    description:
+      "If Cold is afflicted twice, the Temtem becomes Frozen. Frozen Temtem cannot attack, but they can rest normally. Cold and Frozen also serve to erase the Burnt Condition from a Temtem, while Burnt erases both Cold and Frozen",
+    icon: "/images/icons/conditions/Cold.png"
+  }
+];
 
-export const techniqueExample = [{
-  "name": "Crystal Dust",
-  "wikiUrl": "https://temtem.gamepedia.com/Crystal_Dust",
-  "type": "Crystal",
-  "class": "special",
-  "damage": 60,
-  "staminaCost": 11,
-  "hold": 0,
-  "priority": "high",
-  "syngery": "Wind",
-  "synergyEffect": 20,
-  "targets": "Single Other Target",
-  "description": "A miniature version of the \"star-rain\" that is said to have spawned the Archipelago."
-}];
+export const techniqueExample = [
+  {
+    name: "Crystal Dust",
+    wikiUrl: "https://temtem.gamepedia.com/Crystal_Dust",
+    type: "Crystal",
+    class: "special",
+    damage: 60,
+    staminaCost: 11,
+    hold: 0,
+    priority: "high",
+    syngery: "Wind",
+    synergyEffect: 20,
+    targets: "Single Other Target",
+    description:
+      'A miniature version of the "star-rain" that is said to have spawned the Archipelago.'
+  }
+];
 
-export const traitExample = [{
-  "name": "Aerobic",
-  "wikiUrl": "https://temtem.gamepedia.com/Aerobic",
-  "description": "When attacking with Wind techniques, gets SPDEF- and SPD +."
-}];
+export const traitExample = [
+  {
+    name: "Aerobic",
+    wikiUrl: "https://temtem.gamepedia.com/Aerobic",
+    description: "When attacking with Wind techniques, gets SPDEF- and SPD +."
+  }
+];
 
-export const gearExample = [{
-  "name": "Aggressive DNA Strand",
-  "wikiUrl": "https://temtem.gamepedia.com/Aggressive_DNA_Strand",
-  "wikiIconUrl": "https://gamepedia.cursecdn.com/temtem_gamepedia_en/8/8f/AggressiveDNAStrand.png",
-  "icon": "/images/icons/gear/AggressiveDNAStrand.png",
-  "category": "Gear",
-  "consumable": true,
-  "limitedQuantity": false,
-  "purchasable": true,
-  "buyPrice": 10,
-  "description": "Aggressive DNA Strand This single-use DNA strand ensures the egg inherits the parent's ATK.",
-  "gameDescription": "NEEDS DESCRIPTION"
-}];
+export const gearExample = [
+  {
+    name: "Aggressive DNA Strand",
+    wikiUrl: "https://temtem.gamepedia.com/Aggressive_DNA_Strand",
+    wikiIconUrl:
+      "https://gamepedia.cursecdn.com/temtem_gamepedia_en/8/8f/AggressiveDNAStrand.png",
+    icon: "/images/icons/gear/AggressiveDNAStrand.png",
+    category: "Gear",
+    consumable: true,
+    limitedQuantity: false,
+    purchasable: true,
+    buyPrice: 10,
+    description:
+      "Aggressive DNA Strand This single-use DNA strand ensures the egg inherits the parent's ATK.",
+    gameDescription: "NEEDS DESCRIPTION"
+  }
+];
 
-export const knownTemtemExample = [ {
-  "number": 113,
-  "name": "Ganki",
-  "types": [
-    "Electric",
-    "Wind"
-  ],
-  "portraitWikiUrl": "https://gamepedia.cursecdn.com/temtem_gamepedia_en/thumb/b/bc/Ganki.png/50px-Ganki.png",
-  "wikiUrl": "https://temtem.gamepedia.com/Ganki",
-  "stats": {
-    "hp": 38,
-    "sta": 46,
-    "spd": 61,
-    "atk": 57,
-    "def": 38,
-    "spatk": 62,
-    "spdef": 73,
-    "total": 375
-  },
-  "traits": [
-    "Botanophobia",
-    "Cold-Natured"
-  ],
-  "details": {
-    "height": {
-      "cm": 105,
-      "inches": 41
+export const knownTemtemExample = [
+  {
+    number: 113,
+    name: "Ganki",
+    types: ["Electric", "Wind"],
+    portraitWikiUrl:
+      "https://gamepedia.cursecdn.com/temtem_gamepedia_en/thumb/b/bc/Ganki.png/50px-Ganki.png",
+    wikiUrl: "https://temtem.gamepedia.com/Ganki",
+    stats: {
+      hp: 38,
+      sta: 46,
+      spd: 61,
+      atk: 57,
+      def: 38,
+      spatk: 62,
+      spdef: 73,
+      total: 375
     },
-    "weight": {
-      "kg": 14,
-      "lbs": 31
-    }
-  },
-  "techniques": [
-    {
-      "name": "Sparks",
-      "source": "Levelling"
+    traits: ["Botanophobia", "Cold-Natured"],
+    details: {
+      height: {
+        cm: 105,
+        inches: 41
+      },
+      weight: {
+        kg: 14,
+        lbs: 31
+      }
     },
-    {
-      "name": "Nimble",
-      "source": "Levelling"
-    },
-    {
-      "name": "Wind Blade",
-      "source": "Levelling"
-    },
-    {
-      "name": "DC Beam",
-      "source": "Levelling"
-    },
-    {
-      "name": "Chain Lightning",
-      "source": "Levelling"
-    },
-    {
-      "name": "Drill Impact",
-      "source": "Levelling"
-    },
-    {
-      "name": "Electric Storm",
-      "source": "Levelling"
-    },
-    {
-      "name": "Turbo Choreography",
-      "source": "TechniqueCourses"
-    },
-    {
-      "name": "Misogi",
-      "source": "TechniqueCourses"
-    },
-    {
-      "name": "Noxious Bomb",
-      "source": "TechniqueCourses"
-    },
-    {
-      "name": "Tesla Prison",
-      "source": "Breeding"
-    }
-  ],
-  "trivia": [
-    "Ganki was first revealed via Twitter.",
-    "Ganki is a combination of ga (moth) + denki (electric current), both being Japanese words.",
-    "Ganki has an emote on the official Temtem discord.",
-    "Prior to alpha 0.2.5 Ganki learned hypnosis."
-  ],
-  "evolution": {
-    "stage": 1,
-    "evolutionTree": [
+    techniques: [
       {
-        "number": 113,
-        "name": "Ganki",
-        "stage": 1,
-        "levels": 27
+        name: "Sparks",
+        source: "Levelling"
       },
       {
-        "number": 114,
-        "name": "Gazuma",
-        "stage": 2
+        name: "Nimble",
+        source: "Levelling"
+      },
+      {
+        name: "Wind Blade",
+        source: "Levelling"
+      },
+      {
+        name: "DC Beam",
+        source: "Levelling"
+      },
+      {
+        name: "Chain Lightning",
+        source: "Levelling"
+      },
+      {
+        name: "Drill Impact",
+        source: "Levelling"
+      },
+      {
+        name: "Electric Storm",
+        source: "Levelling"
+      },
+      {
+        name: "Turbo Choreography",
+        source: "TechniqueCourses"
+      },
+      {
+        name: "Misogi",
+        source: "TechniqueCourses"
+      },
+      {
+        name: "Noxious Bomb",
+        source: "TechniqueCourses"
+      },
+      {
+        name: "Tesla Prison",
+        source: "Breeding"
       }
     ],
-    "evolves": true,
-    "type": "level"
-  },
-  "wikiPortraitUrlLarge": "https://gamepedia.cursecdn.com/temtem_gamepedia_en/thumb/b/bc/Ganki.png/250px-Ganki.png?version=d18706728e9cc3706caea19e24063ac4",
-  "icon": "/images/portraits/temtem/large/Ganki.png"
-}];
+    trivia: [
+      "Ganki was first revealed via Twitter.",
+      "Ganki is a combination of ga (moth) + denki (electric current), both being Japanese words.",
+      "Ganki has an emote on the official Temtem discord.",
+      "Prior to alpha 0.2.5 Ganki learned hypnosis."
+    ],
+    evolution: {
+      stage: 1,
+      evolutionTree: [
+        {
+          number: 113,
+          name: "Ganki",
+          stage: 1,
+          levels: 27
+        },
+        {
+          number: 114,
+          name: "Gazuma",
+          stage: 2
+        }
+      ],
+      evolves: true,
+      type: "level"
+    },
+    wikiPortraitUrlLarge:
+      "https://gamepedia.cursecdn.com/temtem_gamepedia_en/thumb/b/bc/Ganki.png/250px-Ganki.png?version=d18706728e9cc3706caea19e24063ac4",
+    icon: "/images/portraits/temtem/large/Ganki.png"
+  }
+];
 
 export const weaknessesExample = {
-  "Neutral": {
-    "Neutral": 1,
-    "Fire": 1,
-    "Water": 1,
-    "Nature": 1,
-    "Electric": 1,
-    "Earth": 1,
-    "Mental": 0.5,
-    "Wind": 1,
-    "Digital": 1,
-    "Melee": 1,
-    "Crystal": 1,
-    "Toxic": 1
+  Neutral: {
+    Neutral: 1,
+    Fire: 1,
+    Water: 1,
+    Nature: 1,
+    Electric: 1,
+    Earth: 1,
+    Mental: 0.5,
+    Wind: 1,
+    Digital: 1,
+    Melee: 1,
+    Crystal: 1,
+    Toxic: 1
   },
-  "Fire": {
-    "Neutral": 1,
-    "Fire": 0.5,
-    "Water": 0.5,
-    "Nature": 2,
-    "Electric": 1,
-    "Earth": 0.5,
-    "Mental": 1,
-    "Wind": 1,
-    "Digital": 1,
-    "Melee": 1,
-    "Crystal": 2,
-    "Toxic": 1
+  Fire: {
+    Neutral: 1,
+    Fire: 0.5,
+    Water: 0.5,
+    Nature: 2,
+    Electric: 1,
+    Earth: 0.5,
+    Mental: 1,
+    Wind: 1,
+    Digital: 1,
+    Melee: 1,
+    Crystal: 2,
+    Toxic: 1
   },
-  "Water": {
-    "Neutral": 1,
-    "Fire": 2,
-    "Water": 0.5,
-    "Nature": 0.5,
-    "Electric": 1,
-    "Earth": 2,
-    "Mental": 1,
-    "Wind": 1,
-    "Digital": 2,
-    "Melee": 1,
-    "Crystal": 1,
-    "Toxic": 0.5
+  Water: {
+    Neutral: 1,
+    Fire: 2,
+    Water: 0.5,
+    Nature: 0.5,
+    Electric: 1,
+    Earth: 2,
+    Mental: 1,
+    Wind: 1,
+    Digital: 2,
+    Melee: 1,
+    Crystal: 1,
+    Toxic: 0.5
   },
-  "Nature": {
-    "Neutral": 1,
-    "Fire": 0.5,
-    "Water": 2,
-    "Nature": 0.5,
-    "Electric": 1,
-    "Earth": 2,
-    "Mental": 1,
-    "Wind": 1,
-    "Digital": 1,
-    "Melee": 1,
-    "Crystal": 1,
-    "Toxic": 0.5
+  Nature: {
+    Neutral: 1,
+    Fire: 0.5,
+    Water: 2,
+    Nature: 0.5,
+    Electric: 1,
+    Earth: 2,
+    Mental: 1,
+    Wind: 1,
+    Digital: 1,
+    Melee: 1,
+    Crystal: 1,
+    Toxic: 0.5
   },
-  "Electric": {
-    "Neutral": 1,
-    "Fire": 1,
-    "Water": 2,
-    "Nature": 0.5,
-    "Electric": 0.5,
-    "Earth": 0.5,
-    "Mental": 2,
-    "Wind": 2,
-    "Digital": 2,
-    "Melee": 1,
-    "Crystal": 0.5,
-    "Toxic": 1
+  Electric: {
+    Neutral: 1,
+    Fire: 1,
+    Water: 2,
+    Nature: 0.5,
+    Electric: 0.5,
+    Earth: 0.5,
+    Mental: 2,
+    Wind: 2,
+    Digital: 2,
+    Melee: 1,
+    Crystal: 0.5,
+    Toxic: 1
   },
-  "Earth": {
-    "Neutral": 1,
-    "Fire": 2,
-    "Water": 0.5,
-    "Nature": 0.5,
-    "Electric": 2,
-    "Earth": 1,
-    "Mental": 1,
-    "Wind": 0.5,
-    "Digital": 1,
-    "Melee": 1,
-    "Crystal": 2,
-    "Toxic": 1
+  Earth: {
+    Neutral: 1,
+    Fire: 2,
+    Water: 0.5,
+    Nature: 0.5,
+    Electric: 2,
+    Earth: 1,
+    Mental: 1,
+    Wind: 0.5,
+    Digital: 1,
+    Melee: 1,
+    Crystal: 2,
+    Toxic: 1
   },
-  "Mental": {
-    "Neutral": 2,
-    "Fire": 1,
-    "Water": 1,
-    "Nature": 1,
-    "Electric": 1,
-    "Earth": 1,
-    "Mental": 1,
-    "Wind": 1,
-    "Digital": 1,
-    "Melee": 2,
-    "Crystal": 0.5,
-    "Toxic": 1
+  Mental: {
+    Neutral: 2,
+    Fire: 1,
+    Water: 1,
+    Nature: 1,
+    Electric: 1,
+    Earth: 1,
+    Mental: 1,
+    Wind: 1,
+    Digital: 1,
+    Melee: 2,
+    Crystal: 0.5,
+    Toxic: 1
   },
-  "Wind": {
-    "Neutral": 1,
-    "Fire": 1,
-    "Water": 1,
-    "Nature": 1,
-    "Electric": 0.5,
-    "Earth": 1,
-    "Mental": 1,
-    "Wind": 0.5,
-    "Digital": 1,
-    "Melee": 1,
-    "Crystal": 1,
-    "Toxic": 2
+  Wind: {
+    Neutral: 1,
+    Fire: 1,
+    Water: 1,
+    Nature: 1,
+    Electric: 0.5,
+    Earth: 1,
+    Mental: 1,
+    Wind: 0.5,
+    Digital: 1,
+    Melee: 1,
+    Crystal: 1,
+    Toxic: 2
   },
-  "Digital": {
-    "Neutral": 1,
-    "Fire": 1,
-    "Water": 1,
-    "Nature": 1,
-    "Electric": 1,
-    "Earth": 1,
-    "Mental": 2,
-    "Wind": 1,
-    "Digital": 2,
-    "Melee": 2,
-    "Crystal": 1,
-    "Toxic": 1
+  Digital: {
+    Neutral: 1,
+    Fire: 1,
+    Water: 1,
+    Nature: 1,
+    Electric: 1,
+    Earth: 1,
+    Mental: 2,
+    Wind: 1,
+    Digital: 2,
+    Melee: 2,
+    Crystal: 1,
+    Toxic: 1
   },
-  "Melee": {
-    "Neutral": 1,
-    "Fire": 1,
-    "Water": 1,
-    "Nature": 1,
-    "Electric": 1,
-    "Earth": 2,
-    "Mental": 0.5,
-    "Wind": 1,
-    "Digital": 1,
-    "Melee": 0.5,
-    "Crystal": 2,
-    "Toxic": 1
+  Melee: {
+    Neutral: 1,
+    Fire: 1,
+    Water: 1,
+    Nature: 1,
+    Electric: 1,
+    Earth: 2,
+    Mental: 0.5,
+    Wind: 1,
+    Digital: 1,
+    Melee: 0.5,
+    Crystal: 2,
+    Toxic: 1
   },
-  "Crystal": {
-    "Neutral": 1,
-    "Fire": 0.5,
-    "Water": 1,
-    "Nature": 1,
-    "Electric": 2,
-    "Earth": 0.5,
-    "Mental": 2,
-    "Wind": 1,
-    "Digital": 1,
-    "Melee": 1,
-    "Crystal": 1,
-    "Toxic": 1
+  Crystal: {
+    Neutral: 1,
+    Fire: 0.5,
+    Water: 1,
+    Nature: 1,
+    Electric: 2,
+    Earth: 0.5,
+    Mental: 2,
+    Wind: 1,
+    Digital: 1,
+    Melee: 1,
+    Crystal: 1,
+    Toxic: 1
   },
-  "Toxic": {
-    "Neutral": 1,
-    "Fire": 1,
-    "Water": 2,
-    "Nature": 2,
-    "Electric": 1,
-    "Earth": 0.5,
-    "Mental": 1,
-    "Wind": 1,
-    "Digital": 0.5,
-    "Melee": 1,
-    "Crystal": 0.5,
-    "Toxic": 0.5
+  Toxic: {
+    Neutral: 1,
+    Fire: 1,
+    Water: 2,
+    Nature: 2,
+    Electric: 1,
+    Earth: 0.5,
+    Mental: 1,
+    Wind: 1,
+    Digital: 0.5,
+    Melee: 1,
+    Crystal: 0.5,
+    Toxic: 0.5
   }
 };
 
 export const weaknessCalculateExample = {
-  attacking: 'Electric',
-  defending: ['Water', 'Wind'],
+  attacking: "Electric",
+  defending: ["Water", "Wind"],
   modifiers: [2, 2],
   result: 4
-}
+};
 
 export const breedingExample = [];
