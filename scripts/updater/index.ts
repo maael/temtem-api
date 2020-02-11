72c9015b65e172dce7f65878a8721ee07df817b7
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-11 16:16:48 +0000

    fix: 🐛 Make updater check correct folder for codec logs

--- scripts/updater/index.ts
+++ scripts/updater/index.ts
@@ -4,13 +4,13 @@ import * as log from "../util/log";
 (async () => {
   await processFiles("data");
   await processFiles("public");
-  await processFiles("logs");
+  await processFiles("codec-logs");
 })().catch(e => {
   log.error(e);
   throw e;
 });
 
-async function processFiles(type: "data" | "public" | "logs") {
+async function processFiles(type: "data" | "public" | "codec-logs") {
   log.info(`Processing ${type} files`);
   const dataGit = getGit(type);
   const dataFiles = await getStagedDataFiles(dataGit, [], type);
