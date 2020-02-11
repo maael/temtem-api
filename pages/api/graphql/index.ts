b098227d06b98f747a935e02f0d9f5190452ea55
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-02-11 17:43:03 +0000

    feat: 🎸 Add initial graphql endpoint

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,5 +1,33 @@
+import { graphql, buildSchema } from "graphql";
 import cors from "../../../util/cors";
 
-export default cors((req, res) => {
-  res.send("hey");
+const types = require("../../../data/types.json");
+const conditions = require("../../../data/conditions.json");
+
+const schema = buildSchema(`
+  type Type {
+    name: String!
+    icon: String!
+  }
+  type Condition {
+    name: String!
+    description: String!
+    icon: String!
+  }
+  type Query {
+    types: [Type!]!
+    conditions: [Condition!]!
+  }
+`);
+
+const root = {
+  types: () => types,
+  conditions: () => conditions
+};
+
+export default cors(async (req, res) => {
+  const query = req.body.query;
+  const response = await graphql(schema, query, root);
+
+  return res.end(JSON.stringify(response));
 });
