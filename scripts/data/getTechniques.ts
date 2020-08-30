db303bcf8c845ab81e9fa2423cff2da0a872118d
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-08-30 09:45:43 +0100

    fix: 🐛 Handle multiple pages for techniques

--- scripts/data/getTechniques.ts
+++ scripts/data/getTechniques.ts
@@ -10,11 +10,25 @@ export interface Technique {
 
 export default async function getTechniques() {
   log.info("Starting");
+  let techniques: Technique[] = [];
+  let nextPage: string | undefined =
+    "https://temtem.gamepedia.com/Category:Techniques";
+  while (!!nextPage) {
+    const page = await getTechniquesForPage(nextPage);
+    if (page) {
+      techniques = techniques.concat(page.techniques);
+      nextPage = page.nextPage;
+    } else {
+      nextPage = undefined;
+    }
+  }
+  return techniques;
+}
+
+async function getTechniquesForPage(url: string) {
   try {
     log.info("Running");
-    const result = await got(
-      "https://temtem.gamepedia.com/Category:Techniques"
-    );
+    const result = await got(url);
     const $ = cheerio.load(result.body);
     const page = $(".mw-category").last();
     const techniques = typedToArray<Technique>(
@@ -26,8 +40,18 @@ export default async function getTechniques() {
           wikiUrl: `https://temtem.gamepedia.com${$(el).attr("href")}`
         };
       })
-    );
-    return techniques.filter(({ name }) => name !== "Training Course");
+    ).filter(({ name }) => name !== "Training Course");
+    const nextPage = typedToArray<string | undefined>(
+      $("#mw-pages>a").map((_i, el) => {
+        return $(el)
+          .text()
+          .trim()
+          .toLowerCase() === "next page"
+          ? `https://temtem.gamepedia.com${$(el).attr("href")}`
+          : undefined;
+      })
+    ).filter(Boolean)[0];
+    return { techniques, nextPage };
   } catch (e) {
     log.error(e.message);
   }
