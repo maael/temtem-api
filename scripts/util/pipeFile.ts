c68b19ec1727dc9333a17970e7c4a73c7f89f331
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-04-14 21:14:08 +0100

    feat: 🎸 Assets only add new files by default, use -u to update

--- scripts/util/pipeFile.ts
+++ scripts/util/pipeFile.ts
@@ -6,9 +6,30 @@ import got from "got";
 
 const pipeline = promisify(stream.pipeline);
 
+export const baseDir = path.join(__dirname, "..", "..", "public");
+
+const dirCache = {};
+
 export default async function pipeFile(fileUrl: string, p: string[]) {
+  const args = process.argv.slice(2);
+  const isUpdate = args.includes("-u");
+  if (!isUpdate) {
+    const fileName = p.slice(-1)[0];
+    const base = p.slice(0, -1);
+    const baseFolder = path.join(baseDir, ...base);
+    if (!dirCache[baseFolder]) {
+      dirCache[baseFolder] = fs.promises.readdir(baseFolder, "utf-8");
+    }
+    if (
+      (await dirCache[baseFolder]).some(
+        f => f.toLowerCase() === fileName.toLowerCase()
+      )
+    ) {
+      return Promise.resolve();
+    }
+  }
   return pipeline(
     got.stream(fileUrl),
-    fs.createWriteStream(path.join(__dirname, "..", "..", "public", ...p))
+    fs.createWriteStream(path.join(baseDir, ...p))
   );
 }
